fc8d64d648f8d5a894d89adabc09ef1d8c34528b
Ameya Lokre <47865737+dd-alokre@users.noreply.github.com>
2019-08-15 13:39:29 -0700

    pulse test failure will throw up error in CI pipeline (#129)
    * pulse test failure will throw up error in CI pipeline
    
    * making a test fail to check the CI pipeline status
    
    * correct the failing test

--- development/pulse_run.py
+++ development/pulse_run.py
@@ -1,12 +1,9 @@
 import asyncio
-import os
 import requests
-from time import sleep
 from subprocess import Popen
+from time import sleep
 from asyncio import create_subprocess_shell
-from app.commons.config.utils import init_app_config
-
-ENVIRONMENT_KEY = "ENVIRONMENT"
+import sys
 
 
 async def run_pulse_test():
@@ -14,24 +11,33 @@ async def run_pulse_test():
     Run local-server in subprocess and pulse env as well
     """
     # create payment service subprocess in background
-    command_payment_service = "./development/start-local-server.sh -e testing -p 8000 &"
-    process_payment = await create_subprocess_shell(
-        command_payment_service,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
+    try:
+        requests.get(url="http://localhost:8082/health")
+        print(
+            "payment-service is running before already, no need to spawn up a new one"
+        )
+    except requests.exceptions.ConnectionError:
+        print("Starting a new local-server for payment-service")
+        command_payment_service = (
+            "./development/start-local-server.sh -e testing -p 8082 &"
+        )
+        process_payment = await create_subprocess_shell(
+            command_payment_service,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
 
-    # Status
-    print("Starting payment-service, pid=%s", process_payment.pid, flush=True)
+        # Status
+        print("Starting payment-service, pid=%s", process_payment.pid, flush=True)
+        await process_payment.wait()
 
-    await process_payment.wait()
     # this is the number of retries to connect to payment-service. since the wait period is 1 min and wait duration is
     # 5 secs between every retry connect, the number is set to 12.
     retries = 12
     while True:
         retries -= 1
         try:
-            requests.get(url="http://localhost:8000/health")
+            requests.get(url="http://localhost:8082/health")
             command_runpulse = (
                 'export SERVICE_NAME="payment-service"; '
                 'export PULSE_VENV_PATH="/tmp/pulsevenv"; '
@@ -39,6 +45,7 @@ async def run_pulse_test():
                 "source ${PULSE_VENV_PATH}/bin/activate; "
                 'pip3 install --extra-index-url "https://pypi.fury.io/${FURY_TOKEN}/doordash/" --extra-index-url "https://${ARTIFACTORY_USERNAME/@/%40}:${ARTIFACTORY_PASSWORD}@ddartifacts.jfrog.io/ddartifacts/api/pypi/pypi-local/simple/" doordash-pulse; '
                 "cd pulse; "
+                "pip3 install -r requirements.txt; "
                 "pulse --data-file=${PULSE_VENV_PATH}/infra/local/data.yaml --data-file=infra/local/data.yaml"
             )
             process_pulse = Popen(command_runpulse, shell=True, executable="/bin/bash")
@@ -50,23 +57,23 @@ async def run_pulse_test():
                 flush=True,
             )
             process_pulse.communicate()
+            exit_status = process_pulse.returncode
             process_pulse.kill()
 
-            break
+            return exit_status
         except requests.exceptions.ConnectionError:
-            sleep(5)  # waiting for 5 secs
+            # waiting for 5 secs
+            sleep(5)
             if retries < 0:
                 print("Error connecting to Payment service from Pulse service")
                 raise requests.exceptions.ConnectionError
 
 
 async def main():
-    init_app_config()
-    await run_pulse_test()
+    return await run_pulse_test()
 
 
 if __name__ == "__main__":
-    print("Waiting for dependencies until they are ready")
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    print(f"Dependencies are ready for ENVIRONMENT={os.getenv(ENVIRONMENT_KEY)}")
+    return_value = loop.run_until_complete(main())
+    sys.exit(return_value)
