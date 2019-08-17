372d97c707cd375d979513f4f825ec7ca29d551b
Ameya Lokre <47865737+dd-alokre@users.noreply.github.com>
2019-08-16 17:33:47 -0700

    add headers for pulse api calls, change name of payin-client (#150)
    * add headers for pulse api calls, change name of payin-client

--- pulse/tests/payin/__init__.py
+++ pulse/tests/payin/__init__.py
@@ -3,4 +3,4 @@ from ..utils import SERVICE_URI
 
 payin_configuration = Configuration(host=SERVICE_URI)
 payin_configuration.verify_ssl = False
-payin_crud_client = DefaultApi(ApiClient(configuration=payin_configuration))
+payin_client_pulse = DefaultApi(ApiClient(configuration=payin_configuration))
--- pulse/tests/payin/test_payer.py
+++ pulse/tests/payin/test_payer.py
@@ -1,11 +1,13 @@
 import logging
-
+import os
 import requests
 
-from . import payin_crud_client
+from . import payin_client_pulse
 from ..utils import SERVICE_URI
 
 logger = logging.getLogger(__name__)
+
+STAGING_PULSE_API_KEY = os.getenv("API_KEY_PAYMENT_SERVICE", "")
 
 
 def payer_info():
@@ -21,7 +23,7 @@ def payer_info():
 
 def test_payer_creation():  # hit create_payer API and get_payer api
     # create_payer api
-    test_payer = payin_crud_client.create_payer_api_v1_payers_post(
+    test_payer = payin_client_pulse.create_payer_api_v1_payers_post(
         create_payer_request=payer_info()
     )
     assert test_payer["dd_payer_id"] == "test_01"
@@ -30,20 +32,24 @@ def test_payer_creation():  # hit create_payer API and get_payer api
 def test_payer_retrieval():
     # setup
     test_payer = requests.post(
-        url=SERVICE_URI + "/payin/api/v1/payers", json=payer_info()
+        url=SERVICE_URI + "/payin/api/v1/payers",
+        json=payer_info(),
+        headers={"x-api-key": STAGING_PULSE_API_KEY},
     )
     assert test_payer.status_code == 201
 
     # get_payer api
     payer_id = test_payer.json()["id"]
     verify_payer = requests.get(
-        url=SERVICE_URI + "/payin/api/v1/payers/{}".format(payer_id)
+        url=SERVICE_URI + "/payin/api/v1/payers/{}".format(payer_id),
+        headers={"x-api-key": STAGING_PULSE_API_KEY},
     )
     assert verify_payer.status_code == 200
 
     # trying to fetch a payer that does not exist
     verify_payer = requests.get(
-        url=SERVICE_URI + "/payin/api/v1/payers/{}".format("not_valid_id")
+        url=SERVICE_URI + "/payin/api/v1/payers/{}".format("not_valid_id"),
+        headers={"x-api-key": STAGING_PULSE_API_KEY},
     )
     # status_code should be 404, but getting 500 right now
     assert verify_payer.status_code != 200
@@ -52,7 +58,7 @@ def test_payer_retrieval():
 # def test_payer_payment_method_update():
 #     # setup
 #     test_payer = requests.post(
-#         url=SERVICE_URI + "/payin/api/v1/payers", json=payer_info()
+#         url=SERVICE_URI + "/payin/api/v1/payers", json=payer_info(), headers={'x-api-key': STAGING_PULSE_API_KEY}
 #     )
 #     assert test_payer.status_code == 201
 #
@@ -60,6 +66,6 @@ def test_payer_retrieval():
 #     payer_id = test_payer.json()["id"]
 #     update_payer = requests.patch(
 #         url=SERVICE_URI + "/payin/api/v1/payers/{}".format(payer_id),
-#         json={"default_payment_method_id": "fake_id"},
+#         json={"default_payment_method_id": "fake_id"}, headers={'x-api-key': STAGING_PULSE_API_KEY}
 #     )
 #     assert update_payer.status_code == 200
