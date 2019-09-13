6ef40dd72eec57464e989bc1d1ecc635af35621b
Kyle Hsiao <42754679+kkchsiao@users.noreply.github.com>
2019-09-12 21:55:39 -0700

    PAY-3666: [PIS][G1] enable GET endpoints in PROD (#350)
    * workaround fix to load correct runtime namespace by environment.

--- app/commons/runtime.py
+++ app/commons/runtime.py
@@ -1,3 +1,14 @@
 from doordash_lib.runtime import Runtime
+import os
 
-runtime = Runtime(namespace="payment-service", location="/srv/runtime/current")
+
+def _get_runtime_namespace() -> str:
+    # FIXME: better to define separate env var for runtime namespace through helm.
+    return (
+        "payment-service"
+        if os.getenv("ENVIRONMENT", "local") == "prod"
+        else "staging-payment-service"
+    )
+
+
+runtime = Runtime(namespace=_get_runtime_namespace(), location="/srv/runtime/current")
