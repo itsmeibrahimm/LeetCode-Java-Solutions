6d635db76ab1184bc07e6b303d34736c08c61060
Jonathan Pui <eien@users.noreply.github.com>
2019-09-22 23:18:04 -0700

    tests(stats): remove check for stat count (unreliable since it's an a… (#457)
    * tests(stats): remove check for stat count (unreliable since it's an app level test, and other stats can be emitted)

--- app/test_integration/test_stats.py
+++ app/test_integration/test_stats.py
@@ -10,10 +10,6 @@ def test_request_stats(client: TestClient, get_mock_statsd_events):
     client.get("/health")
 
     events: List[Stat] = get_mock_statsd_events()
-    assert (
-        len(events) == 2
-    ), "response and latency stats even when hitting health endpoint"
-
     stat_names: Dict[str, Stat] = {event.stat_name: event for event in events}
 
     response_name = "dd.response.2XX"
