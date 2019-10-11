3832de45a32c0eabf1071fb1ef3a4f6af11c5a07
Arjun Singh <49180087+asingh08@users.noreply.github.com>
2019-10-11 08:18:38 -0700

    PAYIN-92: Update how objects are instantiated in delay capture job. (#577)
    * PAYIN-92: Update how objects are instantiated in delay capture job.

--- app/payin/jobs.py
+++ app/payin/jobs.py
@@ -12,7 +12,11 @@ from app.payin.core.cart_payment.processor import (
     LegacyPaymentInterface,
 )
 from app.payin.core.cart_payment.types import IntentStatus
+from app.payin.core.payer.payer_client import PayerClient
+from app.payin.core.payment_method.payment_method_client import PaymentMethodClient
 from app.payin.repository.cart_payment_repo import CartPaymentRepository
+from app.payin.repository.payer_repo import PayerRepository
+from app.payin.repository.payment_method_repo import PaymentMethodRepository
 
 logger = get_logger("jobs")
 
@@ -40,16 +44,42 @@ async def capture_uncaptured_payment_intents(
     )
     req_context = build_req_context(app_context)
 
-    cart_payment_repo = CartPaymentRepository(app_context)
+    cart_payment_repo = CartPaymentRepository(context=app_context)
+    payer_repo = PayerRepository(context=app_context)
+    payment_method_repo = PaymentMethodRepository(context=app_context)
+
+    payer_client = PayerClient(
+        app_ctxt=app_context,
+        log=req_context.log,
+        payer_repo=payer_repo,
+        stripe_async_client=req_context.stripe_async_client,
+    )
+
+    payment_method_client = PaymentMethodClient(
+        payment_method_repo=payment_method_repo,
+        log=req_context.log,
+        app_ctxt=app_context,
+        stripe_async_client=req_context.stripe_async_client,
+    )
 
     cart_payment_interface = CartPaymentInterface(
-        app_context, req_context, cart_payment_repo
+        app_context=app_context,
+        req_context=req_context,
+        payment_repo=cart_payment_repo,
+        payer_client=payer_client,
+        payment_method_client=payment_method_client,
+        stripe_async_client=req_context.stripe_async_client,
     )
     legacy_payment_interface = LegacyPaymentInterface(
-        app_context, req_context, cart_payment_repo
+        app_context=app_context,
+        req_context=req_context,
+        payment_repo=cart_payment_repo,
+        stripe_async_client=req_context.stripe_async_client,
     )
     cart_payment_processor = CartPaymentProcessor(
-        req_context.log, cart_payment_interface, legacy_payment_interface
+        log=req_context.log,
+        cart_payment_interface=cart_payment_interface,
+        legacy_payment_interface=legacy_payment_interface,
     )
 
     uncaptured_payment_intents = cart_payment_repo.find_payment_intents_that_require_capture(
