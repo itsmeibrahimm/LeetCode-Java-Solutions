5ba06511e069dd67b4167da2171702a73a157916
Kevin Wang <48926401+kevxwan@users.noreply.github.com>
2019-11-12 12:30:46 -0800

    minor update on jobpool test for clarity (#769)
    * minor update on jobpool test for clarity

--- app/commons/test_unit/jobs/test_pool.py
+++ app/commons/test_unit/jobs/test_pool.py
@@ -25,10 +25,10 @@ class TestJobPool:
         await asyncio.sleep(delay)
         pool.resize(size)
 
-    async def print_job(self, ops, n):
-        ops.append(f"entering {n}")
+    async def print_job(self, ops, n, pool: JobPool):
+        ops.append(f"entering {n} pool size {pool.size}")
         await asyncio.sleep(0.1)
-        ops.append(f"leaving {n}")
+        ops.append(f"leaving {n} pool size {pool.size}")
 
     @pytest.mark.asyncio
     async def test_set_size(self):
@@ -41,21 +41,21 @@ class TestJobPool:
         asyncio.create_task(self.update_pool_size(pool=pool, size=1, delay=0.13))
 
         for i in range(1, 7):
-            await pool.spawn(self.print_job(ops, i))
+            await pool.spawn(self.print_job(ops, i, pool))
 
         await pool.join()
 
         assert ops == [
-            "entering 1",
-            "leaving 1",
-            "entering 2",
-            "entering 3",
-            "entering 4",
-            "leaving 2",
-            "leaving 3",
-            "leaving 4",
-            "entering 5",
-            "leaving 5",
-            "entering 6",
-            "leaving 6",
+            "entering 1 pool size 1",
+            "leaving 1 pool size 1",
+            "entering 2 pool size 1",  # task 2 enqueued when pool size = 1
+            "entering 3 pool size 3",  # task 3, 4 enqueued together after being unblocked by larger pool size
+            "entering 4 pool size 3",
+            "leaving 2 pool size 1",  # when task 2 finished, pool size already reduced to 1
+            "leaving 3 pool size 1",
+            "leaving 4 pool size 1",
+            "entering 5 pool size 1",
+            "leaving 5 pool size 1",
+            "entering 6 pool size 1",
+            "leaving 6 pool size 1",
         ]
