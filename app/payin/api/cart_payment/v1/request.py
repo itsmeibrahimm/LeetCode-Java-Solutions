6a68dc2c5ca00c987906e85d3fd3ab182eb9ebf5
Kevin Wang <48926401+kevxwan@users.noreply.github.com>
2020-01-27 15:12:21 -0800

    [PAYIN-319-2] revise create cp v1 with token to include gateway pgp code (#1053)
    * [PAYIN-319-2] revise create cp v1 with token to include gateway pgp code

--- app/payin/api/cart_payment/v1/request.py
+++ app/payin/api/cart_payment/v1/request.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from pydantic import BaseModel, Schema
 
+from app.commons.types import PgpCode
 from app.payin.api.cart_payment.base.request import (
     ClientDescription,
     CreateCartPaymentBaseRequest,
@@ -12,8 +13,22 @@ from app.payin.core.payer.model import PayerCorrelationIds
 
 
 class CorrelationIds(BaseModel):
-    reference_id: str
-    reference_type: str
+    reference_id: str = Schema(  # type: ignore
+        default=..., description="client provided correlation id for the cart payment"
+    )
+    reference_type: str = Schema(  # type: ignore
+        default=..., description="client provided correlation id type"
+    )
+
+
+class PaymentMethodToken(BaseModel):
+    token: str = Schema(  # type: ignore
+        default=...,
+        description="one-time token to identify a payment method tokenized by payment gateway",
+    )
+    payment_gateway: PgpCode = Schema(  # type: ignore
+        default=..., description="payment gateway which tokenized this payment method"
+    )
 
 
 class CreateCartPaymentRequestV1(CreateCartPaymentBaseRequest):
@@ -23,9 +38,9 @@ class CreateCartPaymentRequestV1(CreateCartPaymentBaseRequest):
     payment_method_id: Optional[UUID] = Schema(  # type: ignore
         default=..., description="id of payment method used for this payment."
     )
-    payment_method_token: Optional[str] = Schema(  # type: ignore
+    payment_method_token: Optional[PaymentMethodToken] = Schema(  # type: ignore
         default=...,
-        description="one-time payment method token issued by payment provider.",
+        description="one-time payment method token issued by payment gateway.",
     )
     dd_stripe_card_id: Optional[int] = Schema(  # type: ignore
         default=..., description="id of legacy stripe card as payment method"
--- app/payin/test_unit/api/test_cart_payment_v1.py
+++ app/payin/test_unit/api/test_cart_payment_v1.py
@@ -1,31 +1,36 @@
-from typing import Optional
+from typing import Optional, Dict, Any
 from unittest.mock import MagicMock
 from uuid import uuid4, UUID
 
 import pytest
 
 from app.payin.api.cart_payment.v1.converter import validate_cart_payment_request_v1
-from app.payin.api.cart_payment.v1.request import CreateCartPaymentRequestV1
+from app.payin.api.cart_payment.v1.request import (
+    CreateCartPaymentRequestV1,
+    PaymentMethodToken,
+)
 
 
 @pytest.mark.parametrize(
     "payment_method_id, payment_method_token, dd_stripe_card_id",
     [
-        (uuid4(), "token", 123),
-        (None, "token", 123),
+        (uuid4(), {"token": "tok", "payment_gateway": "stripe"}, 123),
+        (None, {"token": "tok", "payment_gateway": "stripe"}, 123),
         (uuid4(), None, 123),
-        (uuid4(), "token", None),
+        (uuid4(), {"token": "tok", "payment_gateway": "stripe"}, None),
         (None, None, None),
     ],
 )
 def test_create_cart_payment_v1_request_validation_payment_method(
     payment_method_id: Optional[UUID],
-    payment_method_token: Optional[str],
+    payment_method_token: Optional[Dict[str, Any]],
     dd_stripe_card_id: Optional[int],
 ):
     invalid_request: CreateCartPaymentRequestV1 = MagicMock(CreateCartPaymentRequestV1)
     invalid_request.payment_method_id = payment_method_id
-    invalid_request.payment_method_token = payment_method_token
+    invalid_request.payment_method_token = (
+        PaymentMethodToken(**payment_method_token) if payment_method_token else None
+    )
     invalid_request.dd_stripe_card_id = dd_stripe_card_id
 
     with pytest.raises(ValueError):
